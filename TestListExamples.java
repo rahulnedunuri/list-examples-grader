45345e0b0528d883b7e9a9e154652f5b0016c879
rahulnedunuri <114321096+rahulnedunuri@users.noreply.github.com>
2022-11-09 15:40:51 -0800

    lab

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,25 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testMerge() {
+    ListExamples listex = new ListExamples();
+    ArrayList<String> arrayList1 = new ArrayList<>();
+    ArrayList<String> arrayList2 = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
+    arrayList1.add("a");
+    arrayList1.add("b");
+    arrayList2.add("c");
+    arrayList2.add("d");
+    List<String> joined = ListExamples.merge(arrayList1, arrayList2);
+    
+    expected.add("a");
+    expected.add("b");
+    expected.add("c");
+    expected.add("d");
+    assertEquals(expected, joined);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,18 @@
 # Create your grading script here
 
 set -e
+CP=.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
 
 rm -rf student-submission
 git clone $1 student-submission
 
+
+if [ -e "student-submission/ListExamples.java" ]
+then
+    echo "ListExamples.java exists on your filesystem."
+    cp ListExamples.java /Documents/Github/list-examples-grader
+
+    javac -cp $CP *.java
+    java -cp $CP org.junit.runner.JUnitCore TestListExamples > error.txt
+
+fi
