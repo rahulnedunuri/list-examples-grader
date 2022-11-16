0d2ea48fa983398c412515cf276ada46ffb5b016
rahulnedunuri <114321096+rahulnedunuri@users.noreply.github.com>
2022-11-16 14:12:27 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -12,19 +12,19 @@ then
 	javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> error.txt
 	if [ -s error.txt ]
 	then
-		echo "Compilation failure"
+		echo "Compiler error"
 		echo "$(cat error.txt)"
 		echo "Exited with error code $?"
 		exit
 	else
-		echo "Compilation success"
+		echo "Comile success"
 		java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
 		if [ "$(grep -o E output.txt | wc -l)" != 0 ]
 		then
-		  echo "Errors!"
+		  echo "JUnit test errors"
 		  echo "$(cat output.txt | tail -n2)"
 		else
-		  echo "No errors!"
+		  echo "No JUnit test errors"
 		fi
 	fi
 else
